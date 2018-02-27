484699600a6cfd1bdf318d297478ee8cee58b406
Maxim <mvshmakov@mail.ru>
2018-02-28 01:47:16 +0300

    heroku X express test

--- server.js
+++ server.js
@@ -1,19 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const packageInfo = require('./package.json');
-
 const app = express();
-app.use(bodyParser.json());
-
-app.get('/', function (req, res) {
-	res.json({ version: packageInfo.version });
-});
-
-var server = app.listen(process.env.PORT, process.env.LOCAL_URL || process.env.HEROKU_URL, () => {
-	const host = server.address().address;
-	const port = server.address().port;
-	console.log('Web server started at http://%s:%s', host, port);
-});
 
 module.exports = (bot) => {
 	if (process.env.NODE_ENV == 'development') {
@@ -23,8 +9,10 @@ module.exports = (bot) => {
 		bot.telegram.setWebhook(`${process.env.HEROKU_URL}/bot${process.env.TOKEN}`);
 		app.use(bot.webhookCallback(`/bot${process.env.TOKEN}`));
 	}
-	app.post('/' + bot.token, (req, res) => {
-		bot.processUpdate(req.body);
-		res.sendStatus(200);
+	app.get('/', (req, res) => {
+		res.send('Hello world!');
+	});
+	app.listen(process.env.PORT, () => {
+		console.log(`Server running on port ${process.env.PORT}`);
 	});
 };
