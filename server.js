5e44699aad507b44e04238b6fe111b4bce471387
Maxim <mvshmakov@mail.ru>
2018-05-30 13:39:58 +0300

    yandex dev verification added

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
@@ -10,9 +11,15 @@ module.exports = (bot) => {
     bot.telegram.setWebhook(`${process.env.HEROKU_URL}/bot${process.env.TOKEN}`);
     app.use(bot.webhookCallback(`/bot${process.env.TOKEN}`));
   }
+
   app.get('/', (req, res) => {
     res.send('Hello world!');
   });
+
+  app.get('/yandex_08bb7e8e4db3bb07.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'yandex_08bb7e8e4db3bb07.html'));
+  });
+  
   app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
   });
